52ffc8ae4d781ef1261e9084db4f9e3465e68fcd
Diana Lim <lim.diana@outlook.com>
2020-05-13 13:12:25 -0700

    Comparing enum in TypeScript vs JavaScript

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -6,10 +6,37 @@ var message = 'Hello World';
 
 log(message);
 */
+//---------------------------------------
+// LET VS VAR KEYWORD COMPARISON CODE
+//---------------------------------------
+/*
 function doSomething() {
-    for (var i = 0; i < 5; i++) {
+    for (let i =0; i <5; i++) {
         console.log(i);
     }
+
     console.log('Finally: ' + i);
 }
+
 doSomething();
+*/
+//---------------------------------------
+// 5 DATA TYPES AND TYPE ANNOTATIONS
+//---------------------------------------
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false]; // Possible, but should be avoided.
+var ColorRed = 0;
+var ColorGreen = 1;
+var ColorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var backgroundColor = Color.Green;
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -7,6 +7,11 @@ var message = 'Hello World';
 log(message);
 */
 
+//---------------------------------------
+// LET VS VAR KEYWORD COMPARISON CODE
+//---------------------------------------
+
+/*
 function doSomething() {
     for (let i =0; i <5; i++) {
         console.log(i);
@@ -16,3 +21,22 @@ function doSomething() {
 }
 
 doSomething();
+*/
+
+//---------------------------------------
+// 5 DATA TYPES AND TYPE ANNOTATIONS
+//---------------------------------------
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1,2,3];
+let f: any[] = [1, true, 'a', false]; // Possible, but should be avoided.
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const ColorBlue = 2;
+
+enum Color {Red = 0, Green = 1, Blue= 2};
+let backgroundColor = Color.Green;
