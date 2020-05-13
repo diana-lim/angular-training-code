ff1d8db28912939aeb8820140b5cd0bdc7d4579d
Diana Lim <lim.diana@outlook.com>
2020-05-13 12:58:19 -0700

    let vs var keyword comparison code

--- ts-hello/main.js
+++ ts-hello/main.js
@@ -1,5 +1,15 @@
-function log(message) {
+/*function log(message) {
     console.log(message);
 }
+
 var message = 'Hello World';
+
 log(message);
+*/
+function doSomething() {
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+    }
+    console.log('Finally: ' + i);
+}
+doSomething();
--- ts-hello/main.ts
+++ ts-hello/main.ts
@@ -1,7 +1,18 @@
-function log(message) {
+/*function log(message) {
     console.log(message);
 }
 
 var message = 'Hello World';
 
 log(message);
+*/
+
+function doSomething() {
+    for (let i =0; i <5; i++) {
+        console.log(i);
+    }
+
+    console.log('Finally: ' + i);
+}
+
+doSomething();
